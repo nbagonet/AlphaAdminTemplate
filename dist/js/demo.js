a1a97720d018e3b9ffaf6f47f73c3f43d02c26e7
wangshuov <nbagonet@gmail.com>
2015-01-06 00:13:51 +0800

    Add Bootstrap Tree

--- dist/js/demo.js
+++ dist/js/demo.js
@@ -3288,3 +3288,29 @@
  /**
   * jQuery treetable end
   */
+
+
+ /**
+  * Bootstarp Tree demo begin
+  */
+ $(function() {
+   if ($(".tree").length > 0) {
+     $(document).ready(function() {
+       $('.tree > ul').attr('role', 'tree').find('ul').attr('role', 'group');
+       $('.tree').find('li:has(ul)').addClass('parent_li').attr('role', 'treeitem').find(' > span').attr('title', 'Collapse this branch').on('click', function(e) {
+         var children = $(this).parent('li.parent_li').find(' > ul > li');
+         if (children.is(':visible')) {
+           children.hide('fast');
+           $(this).attr('title', 'Expand this branch').find(' > i').addClass('icon-plus-sign').removeClass('icon-minus-sign');
+         } else {
+           children.show('fast');
+           $(this).attr('title', 'Collapse this branch').find(' > i').addClass('icon-minus-sign').removeClass('icon-plus-sign');
+         }
+         e.stopPropagation();
+       });
+     });
+   }
+ });
+ /**
+  * Bootstrap Tree demo end
+  */
